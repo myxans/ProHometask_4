64bdbe8ada75a4b62351405e47ea67412a47dbfa
myxans <mushenko1992@gmail.com>
2023-01-24 19:47:10 +0200

    add_remote6

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-let num1 = prompt('Вкажіть 1 число');
-let num2 = prompt('Вкажіть 2 число');
-let num3 = prompt('Вкажіть 3 число');
+let num1 = prompt('3 chisla')
 
-let arr = [];
-
-arr.push(num1, num2, num3);
+let arr = num1.split(',');
 
 let summ = 0;
-const array1 = arr;
 
-array1.forEach(
+arr.forEach(
     (char) => {
         summ += Number(char)
     }
@@ -35,19 +30,21 @@ if (arr[0] < arr[1] && arr[0] < arr[2]) {
     min = arr[2]
 };
 
-alert(`first number is ${arr.at(0)}, last number is ${arr.at(-1)} \n ${arr.join(' + ')} = ${summ} \n max is ${max}, min is ${min}`);
+alert(`first number is ${arr.at(0)}, last number is ${arr.at(-1)} 
+${arr.join(' + ')} = ${summ} 
+max is ${max}, min is ${min}`);
 
-let num = prompt('Please enter one of the numbers which you entered before?');
+let element = prompt('Please enter one of the numbers which you entered before?');
 
-switch (num) {
-    case arr[0]:
-        alert(`Ваше число в списке # ${arr.indexOf + 1}`);
+switch (arr.indexOf(element)) {
+    case arr.indexOf(arr[0]):
+        alert(`Ваше число в списке #1`);
         break;
-    case arr[1]:
-        alert(`Ваше число в списке # ${arr.indexOf + 2}`);
+    case arr.indexOf(arr[1]):
+        alert(`Ваше число в списке #2`);
         break;
-    case arr[2]:
-        alert(`Ваше число в списке # ${arr.indexOf + 3}`);
+    case arr.indexOf(arr[2]):
+        alert(`Ваше число в списке #3`);
         break;
     default:
         alert('Sorry, you did not enter this number!')
